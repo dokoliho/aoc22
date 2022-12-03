e537b9bb98f2e9de18dd5b20fcdcbf53ccc1e580
ohofmann <oliver@dokoliho.de>
2022-12-03 08:26:53 +0100

    Comments added

--- day03.py
+++ day03.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from itertools import islice
 from time import perf_counter as pfc
 
 
@@ -8,10 +9,17 @@ def read_puzzle(filename):
 
 
 def convert_to_priority_lists(puzzle):
+    """
+    Zerlegen der Strings in Listen von Charakters (äußere map)
+    Überführen der Charakter in Prioritäten (innere map)
+    """
     return list(map(lambda it1: list(map(lambda it2: priority(it2), list(it1.strip()))), puzzle))
 
 
 def priority(item):
+    """
+    Umrechnung Character -> Priorität
+    """
     if item.islower():
         return ord(item) - ord('a') + 1
     else:
@@ -19,27 +27,53 @@ def priority(item):
 
 
 def solve1(puzzle):
+    """
+    Lösung Teil eins: Aufsummeren der Prioritäten der doppelten Elemente in den beiden
+    Abteilungen des Rucksacks
+    """
     return sum(list(map(find_common_item_in_rucksack, puzzle)))
 
 
 def find_common_item_in_rucksack(rucksack):
+    """
+    Aufteilen des Rucksacks in zwei Abteilungen
+    Ermitteln des gemeinsamen Elements
+    """
     length = len(rucksack)
     middle_index = length // 2
     return find_common_item_in_lists([rucksack[:middle_index], rucksack[middle_index:]])
 
 
 def find_common_item_in_lists(lists):
+    """
+    Ermitteln der gemeinsamen Elemente in mehreren Listen
+    """
     iset = reduce(lambda a, b: set(a).intersection(set(b)), lists)
     return iset.pop() if len(iset) > 0 else None
 
 
 def solve2(puzzle):
+    """
+    Durchlaufen der Rucksäcke in 3-er Gruppen
+    Ermitteln des gemeinsamen Elements
+    Aufaddieren
+    """
     window_size = 3
-    sum = 0
-    for i in range(0, len(puzzle) - window_size + 1, window_size):
-        window = puzzle[i: i + window_size]
-        sum += find_common_item_in_lists(window)
-    return sum
+    return reduce(lambda value, element: value + find_common_item_in_lists(element), batched(puzzle, window_size), 0)
+
+
+def batched(iterable, n):
+    """
+    Erzeugung eines Generators, der das Iterable in Abschnitten zurückliefert.
+    Die übergebene Sequenz wird mit Hilfe eines Iterators durchlaufen
+    islice holt n Elemente (bei einer Liste immer die ersten, bei einem Iterator aufeinanderfolgende)
+
+    Beim ersten Aufruf des Generators wird er wie eine Funktion ausgeführt, wobei sich yield wie return verhält.
+    Bei jedem weiteren Aufruf wird der Generator nach dem letzten yield fortgesetzt.
+    """
+    it = iter(iterable)
+    while batch := list(islice(it, n)):
+        yield batch
 
 
 def solve(puzzle):
@@ -51,3 +85,4 @@ if __name__ == '__main__':
     puzzle = read_puzzle("data/day3.txt")
     start = pfc()
     print(solve(puzzle), pfc()-start)
+
--- day03_test.py
+++ day03_test.py
@@ -1,12 +1,23 @@
-from day03 import solve
+from day03 import solve1, solve2, convert_to_priority_lists
 
 
-def test_solve():
+def test_solve1():
     puzzle = [  "vJrwpWtwJgWrhcsFMMfFFhFp",
                 "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
                 "PmmdzqPrVvPwwTWBwg",
                 "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
                 "ttgJtRGJQctTZtZT",
                 "CrZsJsPPZsGzwwsLwLmpwMDw"]
-    assert solve(puzzle) == 70
+    puzzle = convert_to_priority_lists(puzzle)
+    assert solve1(puzzle) == 157
 
+
+def test_solve2():
+    puzzle = [  "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg",
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw"]
+    puzzle = convert_to_priority_lists(puzzle)
+    assert solve2(puzzle) == 70

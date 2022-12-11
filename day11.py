f2cbced2fb834f050d5d7fb03ba0e5d7eebf399a
ohofmann <oliver@dokoliho.de>
2022-12-11 07:24:52 +0100

    Day 11 monkey business

--- day11.py
+++ day11.py
@@ -1,4 +1,5 @@
-from typing import List, Any
+from typing import List
+import re
 from time import perf_counter as pfc
 
 
@@ -18,12 +19,54 @@ def solve02(lines: List[str]) -> int:
     return 0
 
 
-def convert(lines: List[str]):
-    """
-    """
-    pass
+
+class Monkey:
+    def __init__(self, lines):
+        if len(lines) != 6:
+            raise Exception("Lines count wrong")
+        m = re.search(r"^Monkey (\d+):$", lines[0].strip())
+        self.num = int(m.group(1))
+        m = re.search(r"Starting items:(.+)$", lines[1].strip())
+        self.items = list(map(int, m.group(1).split(",")))
+        m = re.search(r"Operation:(.+)$", lines[2].strip())
+        self.operation = m.group(1).strip()
+        m = re.search(r"Test: divisible by (\d+)$", lines[3].strip())
+        self.divisor = int(m.group(1))
+        m = re.search(r"If true: throw to monkey (\d+)$", lines[4].strip())
+        self.true_dest = int(m.group(1))
+        m = re.search(r"If false: throw to monkey (\d+)$", lines[5].strip())
+        self.false_dest = int(m.group(1))
+        if self.true_dest == self.num or self.false_dest == self.num:
+            raise Exception("Throwing to myself")
+        self.count = 0
+
+    def do_business(self, monkeys):
+        while len(self.items) > 0:
+            item = self.items.pop(0)
+            self.count += 1 # one more inspection
+            loc = {'old': item}
+            exec(self.operation, globals(), loc)
+            item = loc['new'] // 3
+            if item % self.divisor == 0:
+                monkeys[self.true_dest].items.append(item)
+            else:
+                monkeys[self.false_dest].items.append(item)
 
 
+def convert(lines: List[str]) -> List[Monkey]:
+    """
+    """
+    return list(map(lambda line_chunks: Monkey(line_chunks), next_n_lines(lines, 6)))
+
+
+def next_n_lines(lines, n):
+    beginn = 0
+    while(True):
+        end = min(beginn+n, len(lines))
+        yield lines[beginn:end]
+        beginn += (n+1)
+        if beginn >= len(lines):
+            break
 
 if __name__ == '__main__':
     lines = read_puzzle("data/day11.txt")
--- day11_test.py
+++ day11_test.py
@@ -1,13 +1,67 @@
 from typing import List
-from day11 import solve01, solve02, convert
+from day11 import solve01, solve02, convert, Monkey
 
 
 def data() -> List[str]:
     return [
+        'Monkey 0:\n',
+        '  Starting items: 79, 98\n',
+        '  Operation: new = old * 19\n',
+        '  Test: divisible by 23\n',
+        '    If true: throw to monkey 2\n',
+        '    If false: throw to monkey 3\n',
+        '\n',
+        'Monkey 1:\n',
+        '  Starting items: 54, 65, 75, 74\n',
+        '  Operation: new = old + 6\n',
+        '  Test: divisible by 19\n',
+        '    If true: throw to monkey 2\n',
+        '    If false: throw to monkey 0\n',
+        '\n',
+        'Monkey 2:\n',
+        '  Starting items: 79, 60, 97\n',
+        '  Operation: new = old * old\n',
+        '  Test: divisible by 13\n',
+        '    If true: throw to monkey 1\n',
+        '    If false: throw to monkey 3\n',
+        '\n',
+        'Monkey 3:\n',
+        '  Starting items: 74\n',
+        '  Operation: new = old + 3\n',
+        '  Test: divisible by 17\n',
+        '    If true: throw to monkey 0\n',
+        '    If false: throw to monkey 1\n',
      ]
 
+
 def test_convert():
-    pass
+    lines = data()
+    monkeys = convert(lines)
+
+    assert len(monkeys) == 4
+
+    assert monkeys[0].num == 0
+    assert monkeys[0].items == [79, 98]
+    assert monkeys[0].operation == "new = old * 19"
+    assert monkeys[0].divisor == 23
+    assert monkeys[0].true_dest == 2
+    assert monkeys[0].false_dest == 3
+
+    assert monkeys[1].num == 1
+    assert monkeys[1].items == [54, 65, 75, 74]
+    assert monkeys[1].operation == "new = old + 6"
+    assert monkeys[1].divisor == 19
+    assert monkeys[1].true_dest == 2
+    assert monkeys[1].false_dest == 0
+
+
+def test_do_business():
+    lines = data()
+    monkeys = convert(lines)
+    monkeys[0].do_business(monkeys)
+    assert monkeys[0].count == 2
+    assert monkeys[0].items == []
+    assert monkeys[3].items == [74, 500, 620]
 
 
 

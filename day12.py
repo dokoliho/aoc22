f10836bd7baa4d952ea0fb496a31a500265f69fc
ohofmann <oliver@dokoliho.de>
2022-12-12 13:51:09 +0100

    Day 12 solved

--- day12.py
+++ day12.py
@@ -1,6 +1,6 @@
-from functools import reduce
+import math
+from itertools import chain
 from typing import List
-import re
 from time import perf_counter as pfc
 
 
@@ -11,16 +11,166 @@ def read_puzzle(filename: str) -> List[str]:
 
 def solve01(lines: List[str]) -> int:
     """
+    Ermitteln des kürzesten Pfades von S zu E
     """
-    return 0
+    matrix = convert_to_height_matrix(lines)
+    edges = create_edges(matrix)
+    return dijkstra(Node.source_node, Node.destination_node, edges)
 
 
 def solve02(lines: List[str]) -> int:
     """
+    Ermitteln des kürzesten Pfades aus der Menge aller Knoten mit Höhe 0 zum Knoten E
     """
-    return 0
+    matrix = convert_to_height_matrix(lines)
+    edges = create_edges(matrix)
+    min_steps = math.inf
+    for source_node in starting_nodes(matrix):
+        reset_all_costs(matrix)
+        min_steps = min(dijkstra(source_node, Node.destination_node, edges, min_steps), min_steps)
+    return min_steps
 
 
+def convert_to_height_matrix(lines):
+    """
+    Anlegen der Knotenmatrix auf Basis der Inputdaten
+    """
+    return [[Node(row, col, c) for col, c in enumerate(line.strip())] for row, line in enumerate(lines)]
+
+
+def reset_all_costs(matrix):
+    """
+    Setzen der Kosten für alle Knoten der Matrix auf unendlich
+    """
+    for line in matrix:
+        for node in line:
+            node.cost = math.inf
+
+
+class Node:
+    """
+    Klasse für die Repräsentation eines Knoten
+    Attribute:
+    row         : Zeile der Matrix
+    col         : Spalte der Matrix
+    height      : Höhe an dieser Stelle
+    cost        : Kosten für das Erreichen dieser Stelle
+    Außederm gibt es noch die Klassenattribute:
+    source_node         : Ausgangspunkt
+    destination_node    : Ziel
+    """
+
+    source_node = None
+    destination_node = None
+
+    def __init__(self, row, col, char):
+        """
+        Konstruktor
+        """
+        self.row = row
+        self.col = col
+        self.height = self.get_height(char)
+        self.cost = math.inf
+
+    def get_height(self, char):
+        """
+        Ermitteln der Höhe.
+        Bei den Sonderfällen 'S' und 'E' werden die Klassenattribute gesetzt
+        """
+        if char == 'S':
+            char = 'a'
+            Node.source_node = self
+        if char == 'E':
+            char = 'z'
+            Node.destination_node = self
+        return ord(char) - ord('a')
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"Node({self.row}, {self.col}; Height: {self.height})"
+
+
+class Edge:
+    """
+    Klasse für die Repräsentation einer Kante zwischen zwei Knoten
+    """
+
+    def __init__(self, source: Node, destination: Node):
+        """
+        Konstruktor
+        """
+        self.source = source
+        self.destination = destination
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"Edge({self.source.row}, {self.source.col} -> {self.destination.row}, {self.destination.col})"
+
+
+def get_neighbours(matrix, row, col) -> List[Node]:
+    """
+    Ermitteln der max. 4 Nachbarknoten
+    """
+    result = []
+    if row > 0:
+        result.append(matrix[row-1][col])
+    if col > 0:
+        result.append(matrix[row][col-1])
+    if row < len(matrix)-1:
+        result.append(matrix[row+1][col])
+    if col < len(matrix[0])-1:
+        result.append(matrix[row][col+1])
+    return result
+
+
+def create_edges(matrix):
+    """
+    Ermitteln aller zulässigen Wege in der Matrix
+    Es darf max. ein Höhenschritt nach oben ausgeführt werden
+    """
+    edges = []
+    for row, line in enumerate(matrix):
+        for col, node in enumerate(line):
+            node : Node = matrix[row][col]
+            neighbours = get_neighbours(matrix, row, col)
+            for n in neighbours:
+                if n.height <= node.height+1:
+                    edges.append(Edge(node, n))
+    return edges
+
+
+def dijkstra(source: Node, destination: Node, edges: List[Edge], upper_bound=math.inf):
+    """
+    Dijkstra-Algorithmus zum Ermitteln des kürzesten Pfades von source zu destination
+    Da im zweiten Schritt der Algorithmus mehrfach durchgeführt wird, kann über einen
+    optionalen Parameter eine max. Schrittzahl angegeben werden
+    """
+    source.cost = 0
+    border = [source]
+    while destination not in border:
+        border = sorted(border, key=lambda n: n.cost)
+        if len(border) == 0:
+            break
+        node = border.pop(0)
+        if node.cost >= upper_bound:
+            break
+        for edge in edges:
+            if edge.source == node:
+                if edge.destination.cost > edge.source.cost + 1:
+                    edge.destination.cost = edge.source.cost + 1
+                    border.append(edge.destination)
+    return destination.cost
+
+
+def starting_nodes(matrix):
+    """
+    Ermitteln aller Knoten mit Höhe 0
+    """
+    return filter(lambda n: n.height == 0, chain.from_iterable(matrix))
 
 
 if __name__ == '__main__':
--- day12_test.py
+++ day12_test.py
@@ -1,23 +1,56 @@
 from typing import List
-from day12 import solve01, solve02
-
+from day12 import solve01, solve02, convert_to_height_matrix, create_edges, dijkstra, Node
 
 
 def data() -> List[str]:
     return [
-
+        "Sabqponm\n",
+        "abcryxxl\n",
+        "accszExk\n",
+        "acctuvwj\n",
+        "abdefghi\n",
      ]
 
 
+def test_convert_to_matrix():
+    lines = data()
+    matrix = convert_to_height_matrix(lines)
+    source = Node.source_node
+    destination = Node.destination_node
+    assert len(matrix) == 5
+    assert len(matrix[0]) == 8
+    assert source.row == 0
+    assert source.col == 0
+    assert source.height == 0
+    assert destination.row == 2
+    assert destination.col == 5
+    assert destination.height == 25
+
+
+def test_create_edges():
+    lines = data()
+    matrix = convert_to_height_matrix(lines)
+    edges = create_edges(matrix)
+    assert edges[0].source.row == 0
+    assert edges[0].source.col == 0
+    assert edges[0].destination.row == 1
+    assert edges[0].destination.col == 0
+
+
+def test_dijkstra():
+    lines = data()
+    matrix = convert_to_height_matrix(lines)
+    edges = create_edges(matrix)
+    assert dijkstra(Node.source_node, Node.destination_node, edges) == 31
 
 
 def test_solve1():
     lines: List[str] = data()
     result: int = solve01(lines)
-    assert result == 0
+    assert result == 31
 
 
 def test_solve2():
     lines: List[str] = data()
     result: int = solve02(lines)
-    assert result == 0
+    assert result == 29

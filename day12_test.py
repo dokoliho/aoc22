709ce883261ab2e3601085f458991808b97bef88
ohofmann <oliver@dokoliho.de>
2022-12-12 05:50:05 +0100

    Day 12 prepared

---
+++ day12_test.py
@@ -0,0 +1,23 @@
+from typing import List
+from day12 import solve01, solve02
+
+
+
+def data() -> List[str]:
+    return [
+
+     ]
+
+
+
+
+def test_solve1():
+    lines: List[str] = data()
+    result: int = solve01(lines)
+    assert result == 0
+
+
+def test_solve2():
+    lines: List[str] = data()
+    result: int = solve02(lines)
+    assert result == 0

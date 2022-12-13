1aa73f5f71aafeb0d62cbaa222e02d840121fc5b
ohofmann <oliver@dokoliho.de>
2022-12-13 05:15:51 +0100

    Day 13 prepared

---
+++ day13_test.py
@@ -0,0 +1,25 @@
+from typing import List
+from day13 import solve01, solve02
+
+
+def data() -> List[str]:
+    return [
+        "Sabqponm\n",
+        "abcryxxl\n",
+        "accszExk\n",
+        "acctuvwj\n",
+        "abdefghi\n",
+     ]
+
+
+
+def test_solve1():
+    lines: List[str] = data()
+    result: int = solve01(lines)
+    assert result == 0
+
+
+def test_solve2():
+    lines: List[str] = data()
+    result: int = solve02(lines)
+    assert result == 0

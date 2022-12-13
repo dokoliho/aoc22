3be58c5a22cb0bafbe4bc9db382bc753fdd3c805
ohofmann <oliver@dokoliho.de>
2022-12-13 07:31:53 +0100

    Day 13 solved

--- day13.py
+++ day13.py
@@ -1,3 +1,5 @@
+import copy
+import functools
 from typing import List
 from time import perf_counter as pfc
 
@@ -10,19 +12,88 @@ def read_puzzle(filename: str) -> List[str]:
 def solve01(lines: List[str]) -> int:
     """
     """
-    return 0
-
+    pairs = convert_to_packet_pairs(lines)
+    order = list(map(lambda p: check_order(p[0], p[1]), pairs))
+    sum = 0
+    for index, result in enumerate(order):
+        if result:
+            sum += (index+1)
+    return sum
 
 def solve02(lines: List[str]) -> int:
     """
     """
+    divider1 = [[2]]
+    divider2 = [[6]]
+    packets = convert_lines_to_packets(lines)
+    packets.append(divider1)
+    packets.append(divider2)
+    packets = sorted(packets, key=functools.cmp_to_key(compare))
+    index1 = packets.index(divider1)+1
+    index2 = packets.index(divider2)+1
+    return index1*index2
+
+
+def convert_to_packet_pairs(lines):
+    return list(map(lambda pair: (convert_line_to_packet(pair[0]), convert_line_to_packet(pair[1])), convert_to_pairs(lines)))
+
+
+def convert_to_pairs(lines):
+    pairs = []
+    for i in range(0, len(lines), 3):
+        pairs.append((lines[i], lines[i+1]))
+    return pairs
+
+
+def convert_lines_to_packets(lines):
+    packets = []
+    for line in lines:
+        if len(line.strip()) > 0:
+            packets.append(convert_line_to_packet(line))
+    return packets
+
+
+def convert_line_to_packet(line):
+    loc = {}
+    exec(f"packet = {line}", globals(), loc)
+    return loc['packet']
+
+
+def compare(packet1, packet2):
+    p1 = copy.deepcopy(packet1)
+    p2 = copy.deepcopy(packet2)
+    check = check_order(p1, p2)
+    if check:
+        return -1
+    if not check:
+        return 1
     return 0
 
-
+def check_order(packet1, packet2):
+    if  len(packet1) == 0 and len(packet2) == 0:
+        return None
+    if len(packet1) == 0:
+        return True
+    if len(packet2) == 0:
+        return False
+    element1 = packet1.pop(0)
+    element2 = packet2.pop(0)
+    if isinstance(element1, int) and isinstance(element2, int):
+        if element1 == element2:
+            return check_order(packet1, packet2)
+        return element1 < element2
+    if isinstance(element1, int):
+        element1 = [ element1 ]
+    if isinstance(element2, int):
+        element2 = [ element2 ]
+    result = check_order(element1, element2)
+    if result is not None:
+        return result
+    return check_order(packet1, packet2)
 
 
 if __name__ == '__main__':
-    lines = read_puzzle("data/day12.txt")
+    lines = read_puzzle("data/day13.txt")
     start = pfc()
     print(solve01(lines), pfc()-start)
     start = pfc()
--- day13_test.py
+++ day13_test.py
@@ -1,25 +1,83 @@
+import functools
 from typing import List
-from day13 import solve01, solve02
+from day13 import solve01, solve02, convert_to_packet_pairs, check_order, convert_lines_to_packets, compare
 
 
 def data() -> List[str]:
     return [
-        "Sabqponm\n",
-        "abcryxxl\n",
-        "accszExk\n",
-        "acctuvwj\n",
-        "abdefghi\n",
+        '[1,1,3,1,1]\n',
+        '[1,1,5,1,1]\n',
+        '\n',
+        '[[1],[2,3,4]]\n',
+        '[[1],4]\n',
+        '\n',
+        '[9]\n',
+        '[[8,7,6]]\n',
+        '\n',
+        '[[4,4],4,4]\n',
+        '[[4,4],4,4,4]\n',
+        '\n',
+        '[7,7,7,7]\n',
+        '[7,7,7]\n',
+        '\n',
+        '[]\n',
+        '[3]\n',
+        '\n',
+        '[[[]]]\n',
+        '[[]]\n',
+        '\n',
+        '[1,[2,[3,[4,[5,6,7]]]],8,9]\n',
+        '[1,[2,[3,[4,[5,6,0]]]],8,9]\n',
      ]
 
 
+def test_convert_to_packet_pairs():
+    lines = data()
+    result = convert_to_packet_pairs(lines)
+    assert len(result) == 8
+
+
+def test_check_order():
+    lines = data()
+    pairs = convert_to_packet_pairs(lines)
+    result = list(map(lambda p: check_order(p[0], p[1]), pairs))
+    assert result == [True, True, False, True, False, True, False, False]
+
+
+def test_sorted_packages():
+    lines = data()
+    packets = convert_lines_to_packets(lines)
+    packets.append([[2]])
+    packets.append([[6]])
+    packets = sorted(packets, key=functools.cmp_to_key(compare))
+    assert packets == [
+        [],
+        [[]],
+        [[[]]],
+        [1, 1, 3, 1, 1],
+        [1, 1, 5, 1, 1],
+        [[1], [2, 3, 4]],
+        [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+        [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+        [[1], 4],
+        [[2]],
+        [3],
+        [[4, 4], 4, 4],
+        [[4, 4], 4, 4, 4],
+        [[6]],
+        [7, 7, 7],
+        [7, 7, 7, 7],
+        [[8, 7, 6]],
+        [9],
+    ]
 
 def test_solve1():
     lines: List[str] = data()
     result: int = solve01(lines)
-    assert result == 0
+    assert result == 13
 
 
 def test_solve2():
     lines: List[str] = data()
     result: int = solve02(lines)
-    assert result == 0
+    assert result == 140

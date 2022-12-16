ebe41fbc96e6bd1d08f2d2b91ec726aa2a378044
ohofmann <oliver@dokoliho.de>
2022-12-16 06:31:49 +0100

    Day 16 parsing completed

--- day16.py
+++ day16.py
@@ -1,10 +1,26 @@
 from typing import List
+import re
 from time import perf_counter as pfc
 
 
 def read_puzzle(filename: str) -> List[str]:
     with open(filename) as f:
         return [x for x in f]
+
+
+def convert_to_valves(lines):
+    valves = {}
+    for line in lines:
+        name, rate, destinations = convert_line_to_valve(line.strip())
+        valves[name] = (rate, destinations)
+    return valves
+
+
+def convert_line_to_valve(line):
+    m = re.search(r'Valve (\S+) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.*)$', line)
+    name, rate, destinations = m.groups()
+    destinations = list(map(lambda d: d.strip(), destinations.split(",")))
+    return name, int(rate), destinations
 
 
 def solve01(lines: List[str]) -> int:
--- day16_test.py
+++ day16_test.py
@@ -1,17 +1,38 @@
 from typing import List
 from day16 import solve01, solve02
+from day16 import convert_line_to_valve, convert_to_valves
 
 
 def data() -> List[str]:
     return [
-        '498,4 -> 498,6 -> 496,6\n',
-        '503,4 -> 502,4 -> 502,9 -> 494,9\n',
-     ]
+        'Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\n',
+        'Valve BB has flow rate=13; tunnels lead to valves CC, AA\n',
+        'Valve CC has flow rate=2; tunnels lead to valves DD, BB\n',
+        'Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE\n',
+        'Valve EE has flow rate=3; tunnels lead to valves FF, DD\n',
+        'Valve FF has flow rate=0; tunnels lead to valves EE, GG\n',
+        'Valve GG has flow rate=0; tunnels lead to valves FF, HH\n',
+        'Valve HH has flow rate=22; tunnel leads to valve GG\n',
+        'Valve II has flow rate=0; tunnels lead to valves AA, JJ\n',
+        'Valve JJ has flow rate=21; tunnel leads to valve II\n',
+    ]
+
+
+def test_convert_line_to_valve():
+    lines = data()
+    name, rate, destinations = convert_line_to_valve(lines[0].strip())
+    assert name == 'AA'
+    assert rate == 0
+    assert destinations == ['DD', 'II', 'BB']
 
 
 def test_convert():
-    pass
-
+    lines = data()
+    valves = convert_to_valves(lines)
+    assert len(valves) == 10
+    assert 'BB' in valves
+    assert valves['BB'][0] == 13
+    assert valves['BB'][1] == ['CC', 'AA']
 
 
 def test_solve1():

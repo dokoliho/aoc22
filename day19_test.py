571dcede2cea5b74807946cbfdebe741907ec16a
ohofmann <oliver@dokoliho.de>
2022-12-19 05:56:41 +0100

    Day 19 prepared

---
+++ day19_test.py
@@ -0,0 +1,38 @@
+from typing import List
+from day19 import solve01, solve02, convert
+
+
+def data() -> List[str]:
+    return [
+        '2,2,2\n',
+        '1,2,2\n',
+        '3,2,2\n',
+        '2,1,2\n',
+        '2,3,2\n',
+        '2,2,1\n',
+        '2,2,3\n',
+        '2,2,4\n',
+        '2,2,6\n',
+        '1,2,5\n',
+        '3,2,5\n',
+        '2,1,5\n',
+        '2,3,5\n',
+    ]
+
+
+def test_convert():
+    lines = data()
+    scans = convert(lines)
+    assert len(scans) == 0
+
+
+def test_solve1():
+    lines: List[str] = data()
+    result: int = solve01(lines)
+    assert result == 0
+
+
+def test_solve2():
+    lines: List[str] = data()
+    result: int = solve02(lines)
+    assert result == 0

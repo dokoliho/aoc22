4295c80b3223fc5f34b97e7220950617a69a92a5
ohofmann <oliver@dokoliho.de>
2022-12-19 08:45:50 +0100

    Day 19 prepared

--- day19.py
+++ day19.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+import re
 from time import perf_counter as pfc
 
 
@@ -6,7 +7,32 @@ def read_puzzle(filename: str) -> List[str]:
     with open(filename) as f:
         return [x for x in f]
 
+class Blueprint():
 
+    def __init__(self, num, line):
+        self.num = num
+        self.transitions = []
+        m = re.search(r"Each ore robot costs (\d+) ore.", line)
+        ore = int(m.group(1))
+        self.transitions.append(((ore, 0, 0, 0), (1, 0, 0, 0)))
+        m = re.search(r"Each clay robot costs (\d+) ore.", line)
+        ore = int(m.group(1))
+        self.transitions.append(((ore, 0, 0, 0), (0, 1, 0, 0)))
+        m = re.search(r"Each obsidian robot costs (\d+) ore and (\d+) clay.", line)
+        ore = int(m.group(1))
+        clay = int(m.group(2))
+        self.transitions.append(((ore, clay, 0, 0), (0, 0, 1, 0)))
+        m = re.search(r"Each geode robot costs (\d+) ore and (\d+) obsidian.", line)
+        ore = int(m.group(1))
+        obsidian = int(m.group(2))
+        self.transitions.append(((ore, 0, obsidian, 0), (0, 0, 0, 1)))
+        costs = list(map(lambda tup: tup[0], self.transitions))
+        self.max_costs = (
+            max(costs, key=lambda tup: tup[0])[0],
+            max(costs, key=lambda tup: tup[1])[1],
+            max(costs, key=lambda tup: tup[2])[2],
+            max(costs, key=lambda tup: tup[3])[3],
+        )
 
 
 def solve01(lines: List[str]) -> int:
@@ -20,10 +46,64 @@ def solve02(lines: List[str]) -> int:
     """
     return 0
 
+
 def convert(lines):
     """
     """
-    return []
+    result = []
+    for line in lines:
+        token = line.split(":")
+        m = re.search(r"Blueprint (\d+)$", token[0])
+        num = int(m.group(1))
+        result.append(Blueprint(num, token[1]))
+    return result
+
+
+def apply_transition(transition, stock, robots):
+    needed, produced = transition
+    new_stock = []
+    for index, amount in enumerate(needed):
+        if amount > stock[index]:
+            return False, stock, robots
+        new_stock.append(stock[index] - amount)
+    new_robots = list(robots)
+    for index, amount in enumerate(produced):
+        new_robots[index] += amount
+    return True, tuple(new_stock), tuple(new_robots)
+
+
+def apply_blueprint(blueprint, stock, robots, ttl=24):
+    q = [(stock, robots, ttl)]
+    max_geodes = 0
+    while q:
+        print(f"* {q}", flush=True)
+        stock, robots, ttl = q.pop(0)
+        if ttl == 0:
+            max_geodes = max(max_geodes, stock[3])
+            continue
+        new_stock = let_robots_work(stock, robots)
+        append_new_situation(q, new_stock, robots, ttl)
+        for index, transition in enumerate(blueprint.transitions):
+            if robots[index] >= blueprint.max_costs[index] and index != 3:
+                continue        #    Nicht mehr Robots als die Kosten des teuersten Robots
+            if stock[index] > blueprint.max_costs[index] * ttl and index != 3:
+                continue        #    Nicht mehr Robots für etwas, das wir nicht mehr ausgeben können
+            applicable, new_stock, new_robots = apply_transition(transition, stock, robots)
+            if applicable:
+                new_stock = let_robots_work(new_stock, robots)
+                append_new_situation(q, new_stock, new_robots, ttl)
+    return max_geodes
+
+
+def let_robots_work(stock, robots):
+    new_stock = list(stock)
+    for index, amount in enumerate(robots):
+        new_stock[index] += amount
+    return tuple(new_stock)
+
+
+def append_new_situation(queue, stock, robots, ttl):
+    queue.append((stock, robots, ttl - 1))
 
 
 if __name__ == '__main__':
--- day19_test.py
+++ day19_test.py
@@ -1,30 +1,27 @@
 from typing import List
 from day19 import solve01, solve02, convert
-
+from day19 import apply_blueprint
 
 def data() -> List[str]:
     return [
-        '2,2,2\n',
-        '1,2,2\n',
-        '3,2,2\n',
-        '2,1,2\n',
-        '2,3,2\n',
-        '2,2,1\n',
-        '2,2,3\n',
-        '2,2,4\n',
-        '2,2,6\n',
-        '1,2,5\n',
-        '3,2,5\n',
-        '2,1,5\n',
-        '2,3,5\n',
+        'Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.\n',
+        'Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.\n',
     ]
 
 
 def test_convert():
     lines = data()
-    scans = convert(lines)
-    assert len(scans) == 0
+    blueprints = convert(lines)
+    assert len(blueprints) == 2
 
+
+def test_apply_blueprint():
+    lines = data()
+    blueprints = convert(lines)
+    stock = (0, 0, 0, 0)
+    robots = (1, 0, 0, 0)
+    result = apply_blueprint(blueprints[0], stock, robots, ttl=10)
+    assert result == 9
 
 def test_solve1():
     lines: List[str] = data()

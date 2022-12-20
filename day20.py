037e3b0c022ad11be1d582593c40a16ca29ff2c3
ohofmann <oliver@dokoliho.de>
2022-12-20 11:31:50 +0100

    Day 20 part 1 solved Part 2 not working (why?) Got an alternative approach from reddit that works as a benchmark. But my own has to...

--- day20.py
+++ day20.py
@@ -10,20 +10,123 @@ def read_puzzle(filename: str) -> List[str]:
 def solve01(lines: List[str]) -> int:
     """
     """
-    return 0
+    nums = convert(lines)
+    nums = do_all_movements(nums)
+    pos = nums.index(0)
+    return sum([nums[(pos + index) % len(nums)] for index in range(1000, 3001, 1000)])
 
 
 def solve02(lines: List[str]) -> int:
     """
     """
-    return 0
+    nums = convert(lines)
+    dec_nums = apply_decryption_key(nums, 811589153)
+    mixed_nums = do_all_movements(dec_nums, 10)
+    pos = mixed_nums.index(0)
+    return sum([mixed_nums[(pos + index) % len(mixed_nums)] for index in range(1000, 3001, 1000)])
+
+
+def solve01alt(lines: List[str]) -> int:
+    """
+    """
+    nums = convert(lines)
+    nums = alt_impl(nums)
+    pos = nums.index(0)
+    return sum([nums[(pos + index) % len(nums)] for index in range(1000, 3001, 1000)])
+
+
+def solve02alt(lines: List[str]) -> int:
+    """
+    """
+    nums = convert(lines)
+    dec_nums = apply_decryption_key(nums, 811589153)
+    mixed_nums = alt_impl(dec_nums, 10)
+    pos = mixed_nums.index(0)
+    return sum([mixed_nums[(pos + index) % len(mixed_nums)] for index in range(1000, 3001, 1000)])
 
 
 def convert(lines):
     """
     """
-    result = []
-    return result
+    return list(map(lambda l: int(l.strip()), lines))
+
+
+def apply_decryption_key(nums, key):
+    return list(map(lambda n: n * key, nums))
+
+
+def do_all_movements(nums, count=1):
+    offsets = [ 0 for _ in range(len(nums))]
+    for _ in range(count):
+        for index, num in enumerate(nums):
+            do_movement(index, num, offsets)
+    return calc_sequence(nums, offsets)
+
+
+def alt_impl(data, count=1):
+    orig = list(enumerate(data))
+    nums = orig.copy()
+    for _ in range(count):
+        for n in orig:
+            old_idx = nums.index(n)
+            new_idx = (old_idx + n[1]) % (len(nums) - 1)
+            del nums[old_idx]
+            nums.insert(new_idx, n)
+    return list(map(lambda tup: tup[1], nums))
+
+def calc_sequence(nums, offsets):
+    transitions = list(map(lambda tup: (tup[0], (sum(tup)) % len(offsets)), enumerate(offsets)))
+    transitions.sort(key=lambda tup: tup[1])
+    return list(map(lambda t: nums[t[0]], transitions))
+
+
+def check_consistency(nums, offsets):
+    transitions = list(map(lambda tup: (tup[0], (sum(tup)) % len(offsets)), enumerate(offsets)))
+    indx = list(map(lambda tup: tup[1], transitions))
+    indx.sort()
+    for i in range(len(indx)):
+        assert i == indx[i]
+
+
+def do_movement(index, num, offsets):
+    if num == 0:
+        return offsets
+    mod = len(offsets)
+    curr_pos = mod_value(index + offsets[index], mod)
+    next_pos = calc_new_pos(curr_pos, num, mod)
+    for i, offset in enumerate(offsets):
+        if curr_pos < next_pos:
+            if curr_pos < mod_value(i+offset, mod) <= next_pos:
+                offsets[i] -= 1
+        if curr_pos > next_pos:
+            if next_pos <= mod_value(i+offset, mod) < curr_pos:
+                offsets[i] += 1
+    offsets[index] += (next_pos - curr_pos)
+    return offsets
+
+
+def calc_new_pos(current, offset, mod):
+    if offset == 0:
+        return current
+    if offset > 0:
+        return shift_pos(current, offset, mod)
+    else:
+        return shift_neg(current, offset, mod)
+
+
+def shift_pos(current, offset, mod):
+    overflow_count = (current + offset) // mod
+    return (current + offset + overflow_count) % mod
+
+
+def shift_neg(current, offset, mod):
+    x = abs(current + offset - mod)
+    underflow_count = x // mod
+    return (current + offset + underflow_count * mod - underflow_count ) % mod
+
+
+def mod_value(value, mod):
+    return (value + mod) % mod
 
 
 if __name__ == '__main__':
--- day20_test.py
+++ day20_test.py
@@ -1,26 +1,63 @@
 from typing import List
-from day20 import solve01, solve02, convert
+from day20 import solve01, solve02, convert, do_all_movements, do_movement, calc_sequence, apply_decryption_key
+from day20 import alt_impl, solve01alt, solve02alt
 
 def data() -> List[str]:
     return [
-        'Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.\n',
-        'Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.\n',
+        '1\n',
+        '2\n',
+        '-3\n',
+        '3\n',
+        '-2\n',
+        '0\n',
+        '4\n',
     ]
 
 
 def test_convert():
     lines = data()
+    nums = convert(lines)
+    assert len(nums) == 7
+    assert nums[-1] == 4
 
 
+def test_one_encrypted_mix_agaist_alt_impl():
+    lines = data()
+    nums = convert(lines)
+    nums = apply_decryption_key(nums, 811589153)
+    correct = alt_impl(nums)
+    offsets = [ 0 for _ in range(len(nums))]
+    for index, num in enumerate(nums):
+        offsets = do_movement(index, num, offsets)
+    assert calc_sequence(nums, offsets) == correct
+
+
+def test_do_all_movements():
+    lines = data()
+    nums = convert(lines)
+    nums = do_all_movements(nums)
+    assert nums == [1, 2, -3, 4, 0, 3, -2]
 
 
 def test_solve1():
     lines: List[str] = data()
     result: int = solve01(lines)
-    assert result == 0
+    assert result == 3
 
 
 def test_solve2():
     lines: List[str] = data()
     result: int = solve02(lines)
-    assert result == 0
+    assert result == 1623178306
+
+
+def test_solve1_alt():
+    lines: List[str] = data()
+    result: int = solve01alt(lines)
+    assert result == 3
+
+
+def test_solve2_alt():
+    lines: List[str] = data()
+    result: int = solve02alt(lines)
+    assert result == 1623178306
